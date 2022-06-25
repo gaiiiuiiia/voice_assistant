69780a86e67ff454ef63aa8b1ca73da3aef76d5c
maksim.titov <gaiiiuiiialikeaboss@gmail.com>
2022-06-25 16:08:31 +0300

    реализация голосового вывода
     - добавил ALSA в докерфайл
     - для синтеза речи используется pyttsx3, для озвучки голоса pygame

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,14 @@ RUN apt-get update -y \
     && apt-get install -y libespeak-dev portaudio19-dev python3-pyaudio \
     && apt-get install -y libasound-dev libportaudio2 libportaudiocpp0 \
     && apt-get install -y build-essential \
-    && apt-get install -y python3-tk python3-dev
+    && apt-get install -y python3-tk python3-dev \
+    && apt-get install -y alsa-utils \
+    && apt-get install -y ffmpeg
 
 RUN pip install --upgrade pip setuptools
 
 ADD . /app
+ADD ./asound.conf /etc/
 
 RUN pip install --no-cache-dir -r requirements.txt
 
--- app/core/voice_module.py
+++ app/core/voice_module.py
@@ -1,23 +1,59 @@
-from typing import Optional
 import logging
+import os
+from typing import Optional
 
+import pygame.mixer as pgmixer
+import pygame.time as pgtime
 import pyttsx3
 
 import app.config as config
-from app.core.voice_recorder import VoiceRecorder
 from app.core.assistant_manager import AssistantManager
+from app.core.voice_recorder import VoiceRecorder
 
 logger = logging.getLogger(__name__)
 
 
+class Speaker:
+
+    TTS_RATE = 90
+    TEMPORARY_SPEECH_FILE = '__temp.mp3'
+
+    def __init__(self) -> None:
+        self._speaker = self.__init_pyttsx()
+        pgmixer.init()
+
+    def say(self, text: str) -> None:
+        self.__make_sound_file(text)
+        self.__play_sound_file()
+        self.__delete_sound_file()
+
+    def __make_sound_file(self, text: str) -> None:
+        self._speaker.save_to_file(text, self.TEMPORARY_SPEECH_FILE)
+        self._speaker.runAndWait()
+
+    def __play_sound_file(self) -> None:
+        pgtime.wait(100)
+        sounda = pgmixer.Sound(self.TEMPORARY_SPEECH_FILE)
+        sounda.play()
+        pgtime.wait(int(sounda.get_length() * 1000))
+
+    def __delete_sound_file(self) -> None:
+        if os.path.exists(self.TEMPORARY_SPEECH_FILE):
+            os.remove(self.TEMPORARY_SPEECH_FILE)
+
+    def __init_pyttsx(self) -> pyttsx3.Engine:
+        speaker = pyttsx3.init()
+        speaker.setProperty('rate', self.TTS_RATE)
+        speaker.setProperty('voice', 'russian')
+
+        return speaker
+
+
 class VoiceModule:
-
-    TTS_RATE = 150
-
     def __init__(self, assistant_manager: AssistantManager) -> None:
         self._assistant_manager = assistant_manager
         self._voice_recorder = VoiceRecorder()
-        self._speaker = self.__init_pyttsx()
+        self._speaker = Speaker()
 
     def listen(self) -> None:
         logger.info('Запуск службы голосового модуля. Слушаю окружение')
@@ -27,7 +63,7 @@ class VoiceModule:
             logger.info(f'Результат %s', response)
 
             if response:
-                # self._speaker.say(response)
+                self._speaker.say(response)
                 print(f'вход: %s \nвыход: %s' % (context, response))
 
     def _process_context(self, context: str) -> Optional[str]:
@@ -46,12 +82,7 @@ class VoiceModule:
         split_context.pop(0)
         return ' '.join(split_context).strip()
 
-    def __init_pyttsx(self) -> pyttsx3.Engine:
-        speaker = pyttsx3.init()
-        speaker.setProperty('rate', self.TTS_RATE)
-
-        return speaker
-
     def test(self, context: str) -> None:
         response = self._process_context(context)
+        self._speaker.say(response)
         print(f'вход: %s \nвыход: %s' % (context, response))
